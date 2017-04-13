0fd4eb509951a1ef52b824d9a57828fa820f8cc2
Zach Williams <saumanahaii@gmail.com>
2017-04-13 14:54:14 -0400

    Implement API call and initial getWeather function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,29 @@
 //TEMPLATES
+//state object
+let s = {
+  weather: [],
+  weatherAPIKey: 'appid=22bb9867ef69b1e3bad17c9e2f85d1a1',
+  geoKey: 'key=AIzaSyDaLi4kejMskpWXIeVQwXWMviWSoj-ZQOA'
+}
+//state manipulation functions
+sF = {
+getWeather: function(search){
+  $.getJSON(`http://api.openweathermap.org/data/2.5/find?q=${search}&type=like&${s.weatherAPIKey}`,function(data){
+      s.weather = data.list;
+      console.dir(s.weather);
+    });
+
+  }
+}
+
+//view manipulation functions
+vF = {
+  
+}
+
+$('#search-field').keypress(function(event){
+  console.log(event.charCode);
+  if(event.charCode=='13'){
+    sF.getWeather($(this).val())
+  }
+})
